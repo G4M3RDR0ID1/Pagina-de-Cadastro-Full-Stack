42f31ab788fce68566bf029bfce7c806099ab1b6
G4M3RDR0ID1 <gamerdroid33@hotmail.com>
2026-03-12 15:13:02 -0300

    feat: adicionando metodo handleconstraintviolation para erros de validacao do hibernate

--- Back-End/src/main/java/br/com/criandoapi/projeto/exception/GlobalExceptionHandler.java
+++ Back-End/src/main/java/br/com/criandoapi/projeto/exception/GlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package br.com.criandoapi.projeto.exception;
 
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import jakarta.validation.ConstraintViolationException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,9 +14,10 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //Metodo para erros de validaçao
+    // erros de validação do @Valid
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+
         Map<String, String> errors = new HashMap<>();
 
         exception.getBindingResult().getAllErrors().forEach((error) -> {
@@ -29,14 +29,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
-    //Metodo para erros internos
+    // erros de validação do Hibernate
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException exception) {
+
+        String message = exception.getConstraintViolations()
+                .stream()
+                .map(v -> v.getMessage())
+                .findFirst()
+                .orElse("Erro de validação");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", message));
+    }
+
+    // erro genérico
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception exception) {
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of(
                         "error", "Erro interno no servidor",
-                        "message",  exception.getMessage()
+                        "message", exception.getMessage()
                 ));
     }
-
 }
